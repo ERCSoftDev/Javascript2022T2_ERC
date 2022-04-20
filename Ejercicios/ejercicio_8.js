f6553ba4f53e80016404c549360db82890150d39
ENFRY REYES CIPRIAN <58297201+ERCSoftDev@users.noreply.github.com>
2022-04-19 20:34:19 -0400

    Update ejercicio_8.js
    fix exer

--- Ejercicios/ejercicio_8.js
+++ Ejercicios/ejercicio_8.js
@@ -7,11 +7,15 @@ prompt.get(['num1'] , function (err, result){
     let num1 = parseInt(result.num1)
     let resultado;
 
-    for (let index = 2; index < num1 / 2; index++) {
-        if(num1 % index == 0){
+    for (let index = 2; index <= num1 ; index++) {
+        if(num1 % index === 0){
             resultado = num1;
-            console.log('es dividible: ' + resultado);
-        }
+            console.log('es primo: ');
+            return false
+        }else
+            console.log('no es primo: ');
+            return num1 !== 1;
+
 }
    }
 );
